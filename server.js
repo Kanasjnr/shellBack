da33d8b068293e351d3d1992840d083300227dd8
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-10-20 09:00:08 +0100

    update

--- server.js
+++ server.js
@@ -1,28 +1,36 @@
-const express = require('express')
-const app = express()
-const fileUpload = require("express-fileupload")
-const path = require("path")
-const filePayloadExist = require("./middleware/filesPayloadExist")
-const filePayloadExists = require("./middleware/fileExtLimiter")
-const fileExtLimiter = require('./middleware/fileExtLimiter')
-const fileSizeLimiter = require('./middleware/fileSizeLimiter')
+const express = require("express");
+const app = express();
+const fileUpload = require("express-fileupload");
+const path = require("path");
+const filePayloadExist = require("./middleware/filesPayloadExist");
+const filePayloadExists = require("./middleware/fileExtLimiter");
+const fileExtLimiter = require("./middleware/fileExtLimiter");
+const fileSizeLimiter = require("./middleware/fileSizeLimiter");
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname,"index.html"))
-})
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-app.post("/upload",
-fileUpload({createParentPath:true}),
-filePayloadExist,
-fileExtLimiter([".png", ".jpg", ".jpeg"]),
-fileSizeLimiter,
-(req,res) => {
-    const files = req.files
+app.post(
+  "/upload",
+  fileUpload({ createParentPath: true }),
+  filePayloadExist,
+  fileExtLimiter([".png", ".jpg", ".jpeg"]),
+  fileSizeLimiter,
+  (req, res) => {
+    const files = req.files;
     console.log(files);
-    return res.json({status: 'logged', Message:"logged"})
-}
-)
 
+    Object.keys(files).forEach((key) => {
+      const filepath = path.join(__dirname, "files", files[key].name);
+      files[key].mv(filepath, (err) => {
+        if (err) return res.status(500).json({ status: "error", message: err });
+      });
+    });
+
+    return res.json({ status: "logged", Message: "logged" });
+  }
+);
 
 app.listen(PORT, () => console.log(`shellBack running on port ${PORT}`));
